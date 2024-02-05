9e6265f9158ce5de5f9766fbce41b197023f3a63
Airme1 <emmanuelochoja@yahoo.com>
2024-02-05 19:11:32 +0100

    Updating files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-# Preparing server
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-#create the data/web_static structure
-mkdir -p "data/web_static/releases/test" -p "data/web_static/shared"
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-#create html file
-echo "$Hello" > "data/web_static/releases/test/index.html"
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-#create symbolic link
-ln -sfn data/web_static/releases/test/ data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo chown -R ubuntu:ubuntu data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
